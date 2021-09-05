0855099b75b1aa8debaeba5526b6f06b54c75da6
AlexandraTsai <b02105034@gmail.com>
2021-09-05 16:00:14 +0800

    [Feature] Add the pulsing animation.

--- HakkaTranslator/ViewController.swift
+++ HakkaTranslator/ViewController.swift
@@ -20,20 +20,28 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
         binding(viewModel: TranslationViewModel(delegate: self))
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setupPulsatingLayer()
+    }
+
     private let titleLabel = UILabel() --> {
         $0.font = UIFont.systemFont(ofSize: 16)
         $0.textColor = UIColor.lightGray
         $0.text = "Listening..."
+        $0.isHidden = true
     }
     private let micButton = UIButton() --> {
         $0.setImage(#imageLiteral(resourceName: "microphone").withRenderingMode(.alwaysTemplate), for: .normal)
         $0.tintColor = .white
         $0.backgroundColor = .purple
-        $0.layer.cornerRadius = 30
-        $0.imageEdgeInsets = .init(top: 8, left: 8, bottom: 8, right: 8)
+        $0.layer.cornerRadius = 40
+        $0.imageEdgeInsets = .init(top: 20, left: 20, bottom: 20, right: 20)
     }
-    let chineseContainer = UIView()
-    let hakkaContainer = UIView()
+    private let pulsatingLayer = CAShapeLayer()
+    private let bigPulsatingLayer = CAShapeLayer()
+    private let chineseContainer = UIView()
+    private let hakkaContainer = UIView()
     private let chineseLabel = UILabel() --> {
         $0.font = .boldSystemFont(ofSize: 22)
         $0.textColor = .white
@@ -70,12 +78,26 @@ private extension ViewController {
         }
         micButton.snp.makeConstraints {
             $0.centerX.equalToSuperview()
-            $0.bottom.equalToSuperview().inset(50)
-            $0.width.equalTo(60)
-            $0.height.equalTo(60)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-20)
+            $0.width.equalTo(80)
+            $0.height.equalTo(80)
         }
         setupChineseView()
         setupHakkaView()
+    }
+
+    func setupPulsatingLayer() {
+        let bezier = UIBezierPath(arcCenter: .zero, radius: 40, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        [pulsatingLayer, bigPulsatingLayer].forEach {
+            $0.path = bezier.cgPath
+            $0.lineCap = .round
+            $0.position = micButton.center
+            view.layer.addSublayer($0)
+        }
+        pulsatingLayer.fillColor = UIColor.purple.withAlphaComponent(0.8).cgColor
+        bigPulsatingLayer.fillColor = UIColor.purple.withAlphaComponent(0.6).cgColor
+        view.layer.addSublayer(bigPulsatingLayer)
+        view.bringSubviewToFront(micButton)
     }
 
     func setupChineseView() {
@@ -140,22 +162,41 @@ private extension ViewController {
             .disposed(by: disposeBag)
 
         micButton.rx.anyGesture(.longPress())
-            .subscribe(onNext: { [weak viewModel, weak titleLabel] gesture in
+            .subscribe(onNext: { [weak self] gesture in
+                guard let self = self else { return }
                 switch gesture.state {
                 case .began:
-                    titleLabel?.isHidden = false
-                    viewModel?.startRecording()
+                    self.titleLabel.isHidden = false
+                    self.viewModel?.startRecording()
+                    self.pulsatingLayer.addPulsing(toValue: 1.7, forKey: "smallPulsing")
+                    self.bigPulsatingLayer.addPulsing(toValue: 2.1, forKey: "bigPulsing")
                 case .ended:
-                    titleLabel?.isHidden = true
-                    viewModel?.endRecording()
+                    self.titleLabel.isHidden = true
+                    self.viewModel?.endRecording()
+                    self.pulsatingLayer.removeAllAnimations()
+                    self.bigPulsatingLayer.removeAllAnimations()
                 default: break
                 }
             }).disposed(by: disposeBag)
     }
+
+
 }
 
 infix operator -->
 func --> <T>(object: T, closure: (T) -> Void) -> T {
     closure(object)
     return object
+}
+
+extension CAShapeLayer {
+    func addPulsing(toValue: Any, forKey: String) {
+        let animation = CABasicAnimation(keyPath: "transform.scale")
+        animation.toValue = toValue
+        animation.duration = 0.8
+        animation.repeatCount = .infinity
+        animation.timingFunction = .init(name: .easeInEaseOut)
+        animation.autoreverses = true
+        add(animation, forKey: forKey)
+    }
 }
